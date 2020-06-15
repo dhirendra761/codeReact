62c6e5f4edd300ab612ef51780e44af13af87fab
Dhirendra <dhirendra.singh@soprasteria.com>
2020-06-16 02:37:17 +0530

    Define Routes with Route Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import "./index.css";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
