bef7b15d6ea57478dd395e7bf4ae4ea29007b634
Dhirendra <dhirendra.singh@soprasteria.com>
2020-07-06 16:35:13 +0530

    [AMS] Commits -1

--- apps_uf/uf_lan-fr/all_environment/BA_AI_R_X620_ams_windows_UF/bin/web_ping_search_command.py
+++ apps_uf/uf_lan-fr/all_environment/BA_AI_R_X620_ams_windows_UF/bin/web_ping_search_command.py
@@ -1,7 +1,7 @@
 import datetime
 import urllib
 
-urls = ["https://gesundheitsportal.eu.airbus.corp/X620/GE/OH/","https://gesundheitsportal.eu.airbus.corp/X620/GE/Tasks/","https://gesundheitsportal.eu.airbus.corp/X620/GE/Reports/","https://airbusfirstcontact.eu.airbus.corp/X620/UK/OH/","https://airbusfirstcontact.eu.airbus.corp/X620/UK/Tasks","https://airbusfirstcontact.eu.airbus.corp/X620/UK/AMS/","https://ape.eu.airbus.corp/X620/ES/ape/","https://ape.eu.airbus.corp/X620/ES/OHPortal/","https://ape.eu.airbus.corp/X620/ES/Tasks/","https://amsfr-soutiendurantlabsence.eu.airbus.corp/X620/FR/OHPortal/","https://amsfr-soutiendurantlabsence.eu.airbus.corp/X620/FR/AMSFR/"]
+urls = ["https://10.125.88.154/X620/FR/AMSFR/","https://10.125.88.154/X620/FR/OHPortal/","https://gesundheitsportal.eu.airbus.corp/X620/GE/OH/","https://gesundheitsportal.eu.airbus.corp/X620/GE/Tasks/","https://gesundheitsportal.eu.airbus.corp/X620/GE/Reports/","https://airbusfirstcontact.eu.airbus.corp/X620/UK/OH/","https://airbusfirstcontact.eu.airbus.corp/X620/UK/Tasks","https://airbusfirstcontact.eu.airbus.corp/X620/UK/AMS/","https://ape.eu.airbus.corp/X620/ES/ape/","https://ape.eu.airbus.corp/X620/ES/OHPortal/","https://ape.eu.airbus.corp/X620/ES/Tasks/"]
 
 for i, url in enumerate(urls):
     # Do the web-ping
